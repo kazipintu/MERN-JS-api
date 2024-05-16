199b5ec9be44382b87874a18a381710495c207f8
kazipintu <kazipintu@gmail.com>
2024-05-16 19:30:19 +0530

    MERN/JS/api/JSON

--- api.js
+++ api.js
@@ -1,41 +1,9 @@
-{
-  const student = {
-    name: 'A',
-    age: 27,
-    address: 'Bali',
-  }
-  const studentString = JSON.stringify(student);
-  console.log(studentString);
-  const studentParse = JSON.parse(studentString);
-  console.log(studentParse);
+function loadApi() {
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())   //  Most developers use res instead of response
+    .then(data => console.log(data))
+
+  // document.getElementById('show-details').innerText = data.
+
 }
 
-
-
-{
-  const employee = {
-    name: "B",
-    age: 32,
-    id: 445,
-    address: "India"
-  }
-  const employeeString = JSON.stringify(employee);
-  console.log(employeeString);
-  const employeeParse = JSON.parse(employeeString);
-  console.log(employeeParse);
-}
-
-
-
-{
-  const product = {
-    name: "pen",
-    type: "ball",
-    Number: 555,
-    store: 'amazon',
-  }
-  const productString = JSON.stringify(product);
-  console.log(productString);
-  const productParse = JSON.parse(productString);
-  console.log(productParse);
-}
